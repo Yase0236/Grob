86ee3418e9cec44d4ec84338b7b5236129df318b
Jasmin Ersan <yase0236@stud.kea.dk>
2022-03-20 16:05:16 +0100

    ENDELIG detaljer

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+/***********making sure that when you press a link in the hamburger, that the hamburger menu closes and that you go down to that section you clicked on***********/
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
